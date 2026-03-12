1538d50300936c78a41de1ff6dc6d644f59c660f
Joe Churchman <joe@mydecisive.ai>
2026-03-12 09:45:47 -0700

    delete header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import { Forms } from "./components/Forms/Forms";
-import { Header } from "./components/Header";
 import { theme } from "./styles/theme";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header />
       <Forms />
     </ThemeProvider>
   );
