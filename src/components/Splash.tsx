d478f16de9bf7cb2ca130fdfb139d190cb6c9c0c
Joe Churchman <joe@mydecisive.ai>
2026-03-13 11:28:41 -0700

    splash page styles

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -1,6 +1,5 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Divider from "@mui/material/Divider";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 
@@ -14,49 +13,79 @@ export function Splash({ onClickProgress }: { onClickProgress: () => void }) {
           border: "1px solid #CFCFD4",
           backgroundColor: "background.paper",
           minHeight: 620,
+          height: "100%",
+          display: "flex",
+          justifyContent: "stretch",
+          alignItems: "stretch",
         }}
       >
         <Box
           sx={{
-            height: "100%",
+            flex: "1 1 auto",
             display: "grid",
-            gridTemplateColumns: { xs: "1fr", md: "230px 1px 1fr" },
-            gap: 3,
+            gridTemplateColumns: { xs: "1fr", md: "465px 1fr" },
+            gridTemplateRows: "1fr",
+            alignItems: "stretch",
+            justifyContent: "stretch",
           }}
         >
           <Box
             sx={{
+              borderTopLeftRadius: "12px",
+              borderBottomLeftRadius: "12px",
               backgroundColor: "#D9D9D9",
               width: { xs: "none", md: "100%" },
               height: { xs: "none", md: "100%" },
             }}
           />
-          <Divider
-            orientation="vertical"
-            flexItem
+          <Box
             sx={{
-              display: { xs: "none", md: "block" },
-              borderColor: "#D5D5DA",
+              width: "100%",
+              height: "100%",
+              py: 12,
+              px: 8,
+              display: "flex",
+              flexDirection: "column",
+              gap: 2.5,
             }}
-          />
-          <Box sx={{ width: "100%", height: "100%" }}>
-            <Typography variant="h5">Setup a DataDog connection</Typography>
-            <Typography variant="body2">
-              This guided setup will help you accomplish the following:
-              <br />
-              <ul>
-                <li>Connect to Datadog source and preferred destination</li>
-                <li>Configure your collector with ease</li>
-                <li>Ensure your data is</li>
-              </ul>
-            </Typography>
+          >
+            <Box>
+              <Typography variant="h5" sx={{ mb: 1 }}>
+                Setup a DataDog connection
+              </Typography>
+              <Typography component="div" variant="body2">
+                This guided setup will help you accomplish the following:
+                <ul>
+                  <li>Connect to Datadog source and preferred destination</li>
+                  <li>Configure your collector with ease</li>
+                  <li>Ensure your data is...</li>
+                </ul>
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h5" sx={{ mb: 1 }}>
+                Information you need to configure your collector
+              </Typography>
+              <Typography component="div" variant="body2">
+                These items are required to establish and verify your connection
+                <ul>
+                  <li>A target branch to test connection (Argo CD)</li>
+                  <li>API key from Datadog</li>
+                  <li>Destination URL (Datadog or OTLP)</li>
+                </ul>
+              </Typography>
+            </Box>
 
             <Button
               variant="contained"
               size="small"
               type={"button"}
               onClick={onClickProgress}
-              sx={{ alignSelf: "flex-start", textTransform: "none" }}
+              sx={{
+                alignSelf: "flex-start",
+                marginTop: "auto",
+                textTransform: "none",
+              }}
             >
               I'm ready
             </Button>
