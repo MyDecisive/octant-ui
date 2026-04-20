972ffe042af76c1e3e40f7607fe792fa58c59953
Joe Churchman <joe@mydecisive.ai>
2026-04-20 13:38:45 -0700

    sliderControl component

---
+++ src/components/formInputs/SliderControl.tsx
@@ -0,0 +1,33 @@
+import Slider, {
+  type SliderOwnProps as MuiSliderControlProps,
+} from "@mui/material/Slider";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import "./SliderControl.css";
+
+interface SliderControlProps extends MuiSliderControlProps<number> {
+  label: string;
+  valueUnits?: string;
+}
+
+export function SliderControl({
+  label,
+  valueUnits,
+  ...rest
+}: SliderControlProps) {
+  return (
+    <Stack gap={0.5} className="slider-control-container">
+      <Stack
+        direction={"row"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+      >
+        <Typography variant="h6">{label}</Typography>
+        {valueUnits != "" && (
+          <Typography variant="h6">{`${rest.value}${valueUnits}`}</Typography>
+        )}
+      </Stack>
+      <Slider className="slider-control-input" {...rest} />
+    </Stack>
+  );
+}
---
+++ src/stories/SliderControl.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { SliderControl } from "../components/formInputs/SliderControl";
+
+const meta = {
+  title: "Components/SliderControl",
+  component: SliderControl,
+  parameters: {
+    layout: "centered",
+  },
+  args: {},
+} satisfies Meta<typeof SliderControl>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    label: "Volume to filter",
+    valueUnits: "%",
+    value: 0,
+  },
+};
