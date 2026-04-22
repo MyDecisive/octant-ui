b0a1f147e4adb82876fd1d69f29a791dc8b22039
Joe Churchman <joe@mydecisive.ai>
2026-04-22 12:45:37 -0700

    add router dep, svg handling

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,8 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import svgr from "vite-plugin-svgr";
 
 const config: StorybookConfig = {
-  stories: [
-    "../src/stories/**/*.stories.@(ts|tsx|mdx)",
-  ],
+  stories: ["../src/stories/**/*.stories.@(ts|tsx|mdx)"],
   addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-vitest",
@@ -12,6 +11,10 @@ const config: StorybookConfig = {
     "@storybook/addon-onboarding",
   ],
   framework: "@storybook/react-vite",
+  viteFinal: (config) => {
+    config.plugins?.push(svgr());
+    return config;
+  },
 };
 
 export default config;
