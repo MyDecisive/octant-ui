15ce770ba5752136a4a14ce516c8d36a28aff9ee
Joe Churchman <joe@mydecisive.ai>
2026-05-01 09:35:29 -0700

    create richTooltip component, story

---
+++ src/components/RichTooltip.tsx
@@ -0,0 +1,86 @@
+import Stack from "@mui/material/Stack";
+import MuiTooltip, {
+  type TooltipProps as MuiTooltipProps,
+} from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+import { mergeSlotPropsClassNames } from "@utils/mergeSlotPropsClassNames";
+import classNames from "classnames";
+import type { ReactNode } from "react";
+
+interface RichTooltipProps extends Omit<MuiTooltipProps, "title"> {
+  title?: string;
+  description?: string;
+  actions?: ReactNode;
+  titleClassName?: string;
+  descriptionClassName?: string;
+  actionRowClassName?: string;
+}
+
+const baseSlotProps: NonNullable<RichTooltipProps["slotProps"]> = {
+  tooltip: {
+    className: "mdai-rich-tooltip",
+  },
+};
+
+export function RichTooltip({
+  title,
+  description,
+  actions,
+  children,
+  slotProps,
+  titleClassName,
+  descriptionClassName,
+  actionRowClassName,
+  ...rest
+}: RichTooltipProps) {
+  const mergedSlotProps = mergeSlotPropsClassNames<
+    NonNullable<RichTooltipProps["slotProps"]>
+  >(baseSlotProps, slotProps);
+  return (
+    <MuiTooltip
+      arrow
+      {...rest}
+      slotProps={mergedSlotProps}
+      title={
+        <Stack gap={1}>
+          {title && (
+            <Typography
+              className={classNames("mui-rich-tooltip-title", titleClassName)}
+              variant="body2"
+              bold
+            >
+              {title}
+            </Typography>
+          )}
+          {description && (
+            <Typography
+              className={classNames(
+                "mui-rich-tooltip-description",
+                descriptionClassName,
+              )}
+              variant="body2"
+              color="secondary"
+            >
+              {description}
+            </Typography>
+          )}
+          {actions && (
+            <Stack
+              alignItems={"flex-end"}
+              direction={"row"}
+              justifyContent={"flex-end"}
+              className={classNames(
+                "mui-rich-tooltip-actions-row",
+                actionRowClassName,
+              )}
+            >
+              {actions}
+            </Stack>
+          )}
+        </Stack>
+      }
+    >
+      {children}
+    </MuiTooltip>
+  );
+}
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -135,6 +135,12 @@ export const theme = createTheme({
         tooltip: {
           backgroundColor: "#000",
           padding: "8px 12px",
+          "& .MuiStack-root.mui-rich-tooltip-actions-row > .MuiButton-text": {
+            color: "#d500f9",
+          },
+          "& .mui-rich-tooltip-description.MuiTypography-root": {
+            color: "#ffffffb3",
+          },
         },
         arrow: {
           color: "#000",
