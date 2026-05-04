1951f818422c0088645af27e3c341e4bc3371c14
Joe Churchman <joe@mydecisive.ai>
2026-05-04 10:15:41 -0700

    input component manages its own validation state, optional report to parent

--- src/components/formInputs/Input.tsx
+++ src/components/formInputs/Input.tsx
@@ -1,19 +1,52 @@
 import { InputEndAdornment } from "@components/InputEndAdornment";
-import TextField from "@mui/material/TextField";
-import { type ChangeEventHandler, type FocusEventHandler } from "react";
+import TextField, { type TextFieldProps } from "@mui/material/TextField";
+import type { InputValidationErrors } from "@types";
+import debounce from "debounce";
+import {
+  useEffect,
+  useMemo,
+  useState,
+  type ChangeEventHandler,
+  type FocusEventHandler,
+} from "react";
 
-interface InputProps {
-  label?: string;
-  placeholder?: string;
-  value?: string;
-  onChange: ChangeEventHandler<HTMLInputElement>;
-  required?: boolean;
+interface InputProps extends Omit<TextFieldProps<"outlined">, "variant"> {
   tooltip?: string;
   success?: boolean;
-  error?: boolean;
+  validate?: (value?: string) => InputValidationErrors;
+  onValidation?: (error: InputValidationErrors) => void;
+  value?: string;
   helperText?: string;
-  onFocus?: FocusEventHandler<HTMLInputElement>;
-  onBlur?: FocusEventHandler<HTMLInputElement>;
+}
+
+function hasValidationError(maybeErrors: InputValidationErrors | null) {
+  if (maybeErrors == null) {
+    return false;
+  }
+
+  return maybeErrors.length > 0;
+}
+
+function getErrorOrHelperText(
+  validationErrors: InputValidationErrors | null,
+  helperText?: string,
+  errorProp?: boolean,
+) {
+  if (!errorProp && !validationErrors) {
+    return helperText;
+  }
+
+  if (errorProp) {
+    return helperText;
+  }
+
+  if (validationErrors) {
+    return Array.isArray(validationErrors)
+      ? validationErrors[0]
+      : validationErrors;
+  }
+
+  return undefined;
 }
 
 export function Input({
@@ -28,19 +61,63 @@ export function Input({
   onBlur,
   success,
   error,
+  validate,
+  onValidation,
 }: InputProps) {
+  const [validationErrors, setValidationErrors] =
+    useState<InputValidationErrors | null>(null);
+
+  const handleValidate = (value?: string) => {
+    const errors = validate?.(value);
+    setValidationErrors(errors);
+    onValidation?.(errors);
+  };
+
+  const debouncedValidate = useMemo(() => debounce(handleValidate, 400), []);
+
+  const handleBlur: FocusEventHandler<HTMLInputElement> = (e) => {
+    handleValidate(value);
+    onBlur?.(e);
+  };
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const value = e.target.value;
+
+    if (validationErrors) {
+      setValidationErrors(null);
+      onValidation?.(undefined);
+    }
+
+    onChange?.(e);
+
+    if (validationErrors !== null) {
+      debouncedValidate(value);
+    }
+  };
+
+  const fieldError = error ?? hasValidationError(validationErrors);
+
+  const fieldHelperText = getErrorOrHelperText(
+    validationErrors,
+    helperText,
+    error,
+  );
+
+  useEffect(() => () => debouncedValidate.clear(), [debouncedValidate]);
+
   return (
     <TextField
+      variant="outlined"
       label={label}
       value={value}
       placeholder={placeholder}
-      onChange={onChange}
+      onChange={handleChange}
       onFocus={onFocus}
-      onBlur={onBlur}
+      onBlur={handleBlur}
       size="small"
       required={required}
-      helperText={helperText}
-      error={error}
+      helperText={fieldHelperText}
+      error={fieldError}
       fullWidth
       slotProps={{
         input: {
