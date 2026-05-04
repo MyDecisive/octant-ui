e7a2b6ce5b8d45d1d3fd1f4560283fd30930f8b2
Joe Churchman <joe@mydecisive.ai>
2026-05-04 10:17:16 -0700

    integrate changes into connectToCluster

--- src/flows/install/ConnectToCluster.tsx
+++ src/flows/install/ConnectToCluster.tsx
@@ -4,22 +4,25 @@ import { Input } from "@components/formInputs/Input";
 import { FlowCenterColumn } from "@components/layout/FlowCenterColumn";
 import { ViewTitle } from "@components/ViewTitle";
 import { useOctantConnectStore } from "@store";
+import type { InputValidationErrors } from "@types";
 import { useState, type ChangeEventHandler } from "react";
 import { useShallow } from "zustand/shallow";
 import { validateRequired } from "../../fieldValidation/validateRequired";
 import { validateUrlInput } from "../../fieldValidation/validateUrlInput";
 import { argoCdServiceClient } from "../../services/argoCd";
 
-function validateArgoUrl(value: string) {
+function validateArgoUrl(value?: string) {
   const requiredError = validateRequired(value);
   if (requiredError) return requiredError;
 
-  return validateUrlInput(value);
+  return validateUrlInput(value!);
 }
 
 export function ConnectToCluster() {
-  const [urlError, setUrlError] = useState<string | undefined | null>(null);
-  const [tokenError, setTokenError] = useState<string | undefined | null>(null);
+  const [urlError, setUrlError] = useState<InputValidationErrors | null>(null);
+  const [tokenError, setTokenError] = useState<InputValidationErrors | null>(
+    null,
+  );
   const [connectionError, setConnectionError] = useState<string | undefined>();
   const { argoUrl, accountToken } = useOctantConnectStore(
     useShallow((state) => {
@@ -33,29 +36,16 @@ export function ConnectToCluster() {
       };
     }),
   );
-  const advanceInstallFlow = useOctantConnectStore(
-    useShallow((state) => state.advanceInstallFlow),
-  );
   const setFormField = useOctantConnectStore(
     useShallow((state) => state.setFormField),
   );
 
   const handleUrlChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     setFormField("argoUrl", e.target.value);
-    setUrlError(null);
-  };
-
-  const handleUrlValidation = () => {
-    setUrlError(validateArgoUrl(argoUrl));
   };
 
   const handleTokenChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     setFormField("accountToken", e.target.value);
-    setTokenError(null);
-  };
-
-  const handleTokenValidation = () => {
-    setTokenError(validateRequired(accountToken));
   };
 
   const testArgoConnection = async () => {
@@ -66,22 +56,23 @@ export function ConnectToCluster() {
       });
 
       if (result.success) {
-        advanceInstallFlow();
-        return;
+        return true;
       }
       setConnectionError(
         "Token is invalid. Please regenerate your token and try again.",
       );
+      return false;
       // eslint-disable-next-line
     } catch (_) {
       setConnectionError(
         "Oops, we couldn't verify your ArgoCD connection. Please double check your entries and try again.",
       );
+      return false;
     }
   };
 
   return (
-    <FlowCenterColumn>
+    <FlowCenterColumn isForm>
       <ViewTitle
         title="Connect to your Kubernetes Cluster"
         description="<Provide ArgoCD somethings and validate connection>"
@@ -90,40 +81,34 @@ export function ConnectToCluster() {
       <Input
         value={argoUrl}
         onChange={handleUrlChange}
-        onBlur={handleUrlValidation}
-        error={!!urlError}
-        success={urlError === undefined}
+        validate={validateArgoUrl}
+        onValidation={setUrlError}
         label="ArgoCD Cluster URL"
         placeholder="e.g. https://www.main.com"
         tooltip={
           "Target Argo URL is where these changes will live in your version control platform. Please make sure this Argo URL changes as your promote this change through your SDLC environments."
         }
         helperText={
-          urlError ??
           "The dedicated branch that Argo CD will track and sync from"
         }
       />
       <Input
         value={accountToken}
         onChange={handleTokenChange}
-        onBlur={handleTokenValidation}
-        error={!!tokenError}
+        validate={validateRequired}
+        onValidation={setTokenError}
         label="ArgoCD API token"
         placeholder="argocd.token.xxxxxxxx"
         helperText={
-          tokenError ??
           "Generate this in Argo CD under Settings/Accounts/generate token"
         }
       />
-      {connectionError && (
-        <Alert variant="filled" color="error">
-          {connectionError}
-        </Alert>
-      )}
+      {connectionError && <Alert severity="error" title={connectionError} />}
       <AsyncNextButton
         asyncFunction={testArgoConnection}
-        canAsync={tokenError === undefined && urlError === undefined}
+        canAsync={urlError === undefined && tokenError === undefined}
         loadingText={"Connecting"}
+        isSubmit
       />
     </FlowCenterColumn>
   );
