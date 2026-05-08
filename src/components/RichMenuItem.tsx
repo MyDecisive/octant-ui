302e5704397ab713820ef00e6b45f7fece0b139c
Joe Churchman <joe@mydecisive.ai>
2026-05-08 15:19:35 -0700

    pull complex menu item from Select into its own component

---
+++ src/components/RichMenuItem.tsx
@@ -0,0 +1,39 @@
+import Chip, { type ChipProps } from "@mui/material/Chip";
+import MenuItem from "@mui/material/MenuItem";
+import Typography from "@mui/material/Typography";
+import classNames from "classnames";
+
+interface RichMenuItemProps {
+  label?: string;
+  helperText?: string;
+  chip?: ChipProps;
+  value: string;
+  className?: string;
+}
+
+/**
+ * Built to support the various modes of display for options in the Timepicker version of the
+ * Select component, this can be used in other Menus if needed.
+ */
+export function RichMenuItem({
+  label,
+  helperText,
+  chip,
+  value,
+  className,
+}: RichMenuItemProps) {
+  return (
+    <MenuItem
+      className={classNames("mdai-rich-menu-item", className)}
+      value={value}
+    >
+      <span>{label || value}</span>
+      {chip && <Chip {...chip} />}
+      {helperText && (
+        <Typography className="option-helper-text" color="textDisabled">
+          {helperText}
+        </Typography>
+      )}
+    </MenuItem>
+  );
+}
