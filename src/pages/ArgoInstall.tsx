4ace18b23897cb74b2a1610e2e4af8718b904fa0
Joe Churchman <joe@mydecisive.ai>
2026-05-18 14:50:51 -0700

    fix after rebase

--- src/pages/ArgoInstall.tsx
+++ src/pages/ArgoInstall.tsx
@@ -4,7 +4,7 @@ import { ViewTitle } from "@components/ViewTitle";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import { useInstallAndConnectStore } from "@store/installAndConnectStore";
-import { DeployArgoCopy as copy } from "../../copy/install/ArgoInstall.copy";
+import { DeployArgoCopy as copy } from "../copy/install/ArgoInstall.copy";
 
 export function ArgoInstall() {
   const argoAgreement = useInstallAndConnectStore(
