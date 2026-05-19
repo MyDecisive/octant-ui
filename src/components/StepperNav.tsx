a4b3ab5dccc6ceeee6ffd975b266ca9bc6a7b373
Joe Churchman <joe@mydecisive.ai>
2026-05-19 13:46:21 -0700

    fix programmatic existing navigation

--- src/components/StepperNav.tsx
+++ src/components/StepperNav.tsx
@@ -2,26 +2,26 @@ import Box from "@mui/material/Box";
 import Step from "@mui/material/Step";
 import StepButton from "@mui/material/StepButton";
 import Stepper from "@mui/material/Stepper";
-import { useLocation, useParams } from "wouter";
+import { useLocation, useRoute, type DefaultParams } from "wouter";
 import { INSTALL_AND_CONNECT, ROUTES } from "../constants/routing";
 
 export function StepperNav() {
-  const [, params] = useParams<[boolean, { step: string }]>();
+  const [, params] = useRoute<DefaultParams>(ROUTES.INSTALL_STEP);
   const [, navigate] = useLocation();
-  const activeStep = parseInt(params.step);
+  const activeStepIndex = parseInt(params?.step ?? "0") - 1;
 
   return (
     <Box className="left-column">
-      <Stepper activeStep={activeStep} orientation="vertical">
+      <Stepper activeStep={activeStepIndex} orientation="vertical">
         {INSTALL_AND_CONNECT.map(({ label }, index) => {
           return (
-            <Step key={label} completed={index < activeStep}>
+            <Step key={label} completed={index < activeStepIndex}>
               <StepButton
                 color="inherit"
                 onClick={() =>
                   navigate(`${ROUTES.INSTALL}/${(index + 1).toString()}`)
                 }
-                disabled={index > activeStep} // TODO: refactor disabled logic
+                disabled={index > activeStepIndex} // TODO: refactor disabled logic
               >
                 {label}
               </StepButton>
--- src/utils/useAdvanceInstallAndConnect.ts
+++ src/utils/useAdvanceInstallAndConnect.ts
@@ -1,11 +1,11 @@
 import { useCallback } from "react";
-import { useLocation, useParams } from "wouter";
+import { useLocation, useRoute, type DefaultParams } from "wouter";
 import { ROUTES } from "../constants/routing";
 
 export function useAdvanceInstallAndConnect() {
-  const [, params] = useParams<[boolean, { step: string }]>();
+  const [, params] = useRoute<DefaultParams>(ROUTES.INSTALL_STEP);
   const [, navigate] = useLocation();
-  const currentStepNumber = parseInt(params.step);
+  const currentStepNumber = parseInt(params?.step ?? "0");
 
   const advanceInstallAndConnectFlow = useCallback(() => {
     navigate(`${ROUTES.INSTALL}/${currentStepNumber + 1}`);
