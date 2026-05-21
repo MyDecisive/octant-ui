af91a081912585b95bca7a79068a5a17bd227e86
Joe Churchman <joe@mydecisive.ai>
2026-05-21 14:37:54 -0700

    track lastCompletedStep from advance flow handler

--- src/utils/useAdvanceInstallAndConnect.ts
+++ src/utils/useAdvanceInstallAndConnect.ts
@@ -1,3 +1,4 @@
+import { useInstallAndConnectStore } from "@store/installAndConnectStore";
 import { useCallback } from "react";
 import { useLocation, useRoute, type DefaultParams } from "wouter";
 import { ROUTES } from "../constants/routing";
@@ -6,10 +7,12 @@ export function useAdvanceInstallAndConnect() {
   const [, params] = useRoute<DefaultParams>(ROUTES.INSTALL_STEP);
   const [, navigate] = useLocation();
   const currentStepNumber = parseInt(params?.step ?? "0");
+  const setFormField = useInstallAndConnectStore((state) => state.setFormField);
 
   const advanceInstallAndConnectFlow = useCallback(() => {
+    setFormField("lastCompletedStep", currentStepNumber);
     navigate(`${ROUTES.INSTALL}/${currentStepNumber + 1}`);
-  }, [currentStepNumber, navigate]);
+  }, [currentStepNumber, navigate, setFormField]);
 
   return advanceInstallAndConnectFlow;
 }
