1d41f37219596c87e94bc6f99dee622edfc3e14e
Joe Churchman <joe@mydecisive.ai>
2026-06-25 08:39:26 -0700

    add more aliases

--- vite.config.ts
+++ vite.config.ts
@@ -28,10 +28,12 @@ export default defineConfig(({ mode }) => {
       alias: {
         "@components": path.resolve(__dirname, "./src/components"),
         "@constants": path.resolve(__dirname, "./src/constants"),
-        "@utils": path.resolve(__dirname, "./src/utils"),
-        "@app-types": path.resolve(__dirname, "./src/types"),
-        "@store": path.resolve(__dirname, "./src/store"),
+        "@copy": path.resolve(__dirname, "./src/copy"),
+        "@fieldValidation": path.resolve(__dirname, "./src/fieldValidation"),
         "@services": path.resolve(__dirname, "./src/services"),
+        "@store": path.resolve(__dirname, "./src/store"),
+        "@app-types": path.resolve(__dirname, "./src/types"),
+        "@utils": path.resolve(__dirname, "./src/utils"),
       },
     },
   };
